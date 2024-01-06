91b64f52c470c47d0d4e6ff0ad491de74128f10c
sktrader <kswapnil55@gmail.com>
2024-01-06 20:34:31 +0530

    .

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -5,7 +5,7 @@ const HeaderComponent = () => {
     <div>
         <header>
             <nav className='navbar navbar-dark bg-dark'>
-                <a className="navbar-brand" href="https://www.javaguides.net">Employee Management System</a>
+                <a className="navbar-brand" href="https://sktrader.github.io/AlgoWebPro/">Employee Management System</a>
             </nav>
         </header>
     </div>
